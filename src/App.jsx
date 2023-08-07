432479694ffc9c67036604cae9f5e4845b56cecc
Maulana-Iqbal <maulanai957@gmail.com>
2023-08-07 12:16:43 +0700

    add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { TbBrandGithub } from 'react-icons/tb';
 import Button from './components/button';
 import Card from './components/Card';
+import PlaceContentCenter from './components/PlaceContentCenter';
+import { useState } from 'react';
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+		setCount((x) => x + 1);
+	}
 	return (
-		<div className={'bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'}>
+		<div className={'bg-slate-100 text-slate-800 tracking-tight antialiased flex flex-col gap-4 items-center justify-center min-h-screen'}>
 			<div className='max-w-2xl flex items-center gap-4 w-full'>
 				<Card>
 					<Card.Title>Halo React</Card.Title>
@@ -32,6 +39,23 @@ function App() {
 						</Button>
 					</Card.Footer>
 				</Card>
+			</div>
+
+			<div className='max-w-2xl flex items-center gap-4 w-full'>
+				<PlaceContentCenter>
+					<h1 className='text-5xl font-blod'>{count}</h1>
+					<div className='mt-5 flex items-center gap-2'>
+						<Button onClick={handleClick}>+1 </Button>
+						<Button
+							onClick={() => {
+								handleClick();
+								handleClick();
+								handleClick();
+							}}>
+							+3
+						</Button>
+					</div>
+				</PlaceContentCenter>
 			</div>
 		</div>
 	);
---
+++ src/components/PlaceContentCenter.jsx
@@ -0,0 +1,9 @@
+export default function PlaceContentCenter({ children }) {
+	return (
+		<div>
+			<div>
+				<div className=' max-w-lg w-full'>{children}</div>
+			</div>
+		</div>
+	);
+}
